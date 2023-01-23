bed4d3ae238c22f9893ba2cfeb86f3c7ab56cbde
Dhivakaran-Kalanesan <123280511+Dhivakaran-Kalanesan@users.noreply.github.com>
2023-01-23 18:53:31 +0530

    Add files via upload

---
+++ insta.js
@@ -0,0 +1,19 @@
+
+
+
+const heart = document.querySelector('.hs');
+const heart1 = document.querySelector('.hs1');
+const heart2 = document.querySelector('.hs2');
+
+function change(){
+    heart.classList.toggle('cc');
+}
+
+function change1(){
+    heart1.classList.toggle('cc1');
+}
+
+function change2(){
+    heart2.classList.toggle('cc2');
+}
+
